81cb65db4771532f5a53de57a98fa7b608cebd89
hhacker1999 <harsh.sadh313@gmail.com>
2022-06-08 12:32:23 +0530

    create contact model

---
+++ models/contact_model.go
@@ -0,0 +1,8 @@
+package models
+
+type Contact struct {
+	Id        string `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Numbers   []int  `json:"numbers"`
+}
